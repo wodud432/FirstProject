05e73f38e82cd7574fc6fe2cee6bc418b0c863a3
jhta <jhta@L3-05>
2014-04-21 10:30:23 +0900

    tqf

--- test1/src/test1/MainClass.java
+++ test1/src/test1/MainClass.java
@@ -3,7 +3,7 @@ package test1;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Ã¹¹øÂ° ÇÁ·ÎÁ§Æ®");
+		System.out.println("ì²«ë²ˆì§¸ í”„ë¡œì íŠ¸");
 
 	}
 
